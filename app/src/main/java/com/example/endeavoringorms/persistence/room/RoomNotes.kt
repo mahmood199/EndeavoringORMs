c9facf2c4f841d98214d7d8746d97a7d6fb49a17
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-06-24 00:41:23 +0530

    Further configuration of Dao and Entities (#4)
    * Further configuration of Dao and Entities
    
    * Added chart for Conflict Resolution Strategies
    
    * Further configuration of Dao and Entities

---
+++ app/src/main/java/com/example/endeavoringorms/persistence/room/RoomNotes.kt
@@ -0,0 +1,6 @@
+package com.example.endeavoringorms.persistence.room
+
+/**
+ *  Note, though, that many of the annotation attributes
+ *  that Room uses refer to column names, not property names
+ */
--- app/src/main/java/com/example/endeavoringorms/persistence/room/daos/OwnerAndPetDao.kt
+++ app/src/main/java/com/example/endeavoringorms/persistence/room/daos/OwnerAndPetDao.kt
@@ -1,6 +1,9 @@
 package com.example.endeavoringorms.persistence.room.daos
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import com.example.endeavoringorms.persistence.room.entities.Owner
 import com.example.endeavoringorms.persistence.room.entities.OwnerWithPets
 import com.example.endeavoringorms.persistence.room.entities.Pet
@@ -10,11 +13,44 @@ interface OwnerAndPetDao {
     @Query("SELECT * FROM owner")
     suspend fun getAllOwners(): List<Owner>
 
+    @Query("SELECT * FROM owner where ownerId=:ownerId")
+    suspend fun getOwnerById(ownerId: Long): Owner
+
+    @Query("SELECT * FROM owner where ownerId IN (:ids)")
+    suspend fun getOwnersByIds(ids: List<String>): Owner
+
+
     @Query("SELECT * FROM pet")
     suspend fun getAllPets(): List<Pet>
 
+    @Query("SELECT * FROM pet where petId=:petId")
+    suspend fun getPetById(petId: Long): Pet
+
+    @Query("SELECT * FROM pet where petId IN (:ids)")
+    suspend fun getPetsByIds(ids: List<String>): Owner
+
+
     @Query("SELECT * from owner")
     suspend fun getAllOwnerWithPets(): List<OwnerWithPets>
+
+
+    suspend fun insertOwner(owner: Owner): Owner {
+        owner.ownerId = _insertOwner(owner)
+        return owner
+    }
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract suspend fun _insertOwner(owner: Owner): Long
+
+
+    @Insert
+    suspend fun _insertPet(pet: Pet)
+
+    @Transaction
+    suspend fun insert(owner: Owner, pet: Pet) {
+        _insertOwner(owner = owner)
+        _insertPet(pet = pet)
+    }
 
 
 }
