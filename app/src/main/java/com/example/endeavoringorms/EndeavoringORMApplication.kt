3bd0b55581bb9620dfb59079f52971483d0ac6a4
MSI KATANA <gaudentas199@gmail.com>
2023-07-01 23:15:29 +0530

    Added application class for Hilt.
    EndeavoringORMApplication.kt

---
+++ app/src/main/java/com/example/endeavoringorms/EndeavoringORMApplication.kt
@@ -0,0 +1,9 @@
+package com.example.endeavoringorms
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EndeavoringORMApplication : Application() {
+
+}
